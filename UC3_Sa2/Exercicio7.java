c9449a1c64f16cc6f344b808a658f4f77b1265b2
Christofer Alberto Pfeiffer <pfeiffer10.yt@gmail.com>
2024-09-18 09:43:44 -0300

    Add files via upload

---
+++ UC3_Sa2/Exercicio7.java
@@ -0,0 +1,19 @@
+package UC3_Sa2;
+
+public class Exercicio7 {
+    public static void main(String[] args) {
+        String[] desenho = {
+                "    *",
+                "   * *",
+                "  *   *",
+                " *     *",
+                "***   ***",
+                "  *   *",
+                "  *   *",
+                "  *****"
+        };
+        for (String linha : desenho) {
+            System.out.println(linha);
+        }
+    }
+}
